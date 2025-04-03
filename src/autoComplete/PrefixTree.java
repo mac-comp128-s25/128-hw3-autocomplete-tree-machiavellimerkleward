15edc6b0dea4aa8db22e1fca0d35105cf64a86e4
machiavellimerkleward <mmerklew@macalester.edu>
2025-04-02 20:57:09 -0500

    started add()

--- src/autoComplete/PrefixTree.java
+++ src/autoComplete/PrefixTree.java
@@ -1,6 +1,7 @@
 package autoComplete;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -24,7 +25,45 @@ public class PrefixTree {
      * @param word
      */
     public void add(String word){
+        TreeNode parent = root;
+        for(int i = 0; i<word.length()-1; i++){
+            if(parent.children.containsKey(word.charAt(i))){ // not accounting for 
+                if(i == word.length()-1){
+                    parent.children.get(word.charAt(i)).isWord = true;
+                }
+                parent = root.children.get(word.charAt(i)); //sets the current to be the node for character i
+                if(i == word.length()-1){
+                    
+                }
+                continue;
+            }
+            else{
+                TreeNode charNode = new TreeNode();
+                charNode.letter = word.charAt(i);
+                parent.children.put(word.charAt(i), charNode);
+                if(i == word.length()-1){
+                    charNode.isWord = true;
+                }
+                parent = charNode;
+            }
+        }
+        //make a recursive function!!
+        //base case: the letter is the final letter -- add it and unwind the recursion
+        // for each character, check if it's in that node's map
+            //if yes, add(char)
+            //if not, make a new node and add it as the parent's child, then add it to the parent's map of children, then add(next char)
+            //TODO NOT RECURSIVE
         //TODO: complete me
+        /*
+         * create nodes for each character in the tree (if they don't already exist)
+         * link together the nodes at the proper point in the tree and store them in the children maps
+         * when the last character of the word is reached, mark it as the end of the word
+         * if the word is not in the tree, increment size
+         * if the word already exists, don't change anything
+         * use the charAt method
+         */
+
+        
     }
 
     /**
@@ -34,6 +73,11 @@ public class PrefixTree {
      */
     public boolean contains(String word){
         //TODO: complete me
+        /*
+         * return true if the word is contained in the tree
+         * start at root and iterate down through the characters in the tree
+         * if each character is found in the correct order, it passes
+         */
         return false;
     }
 
@@ -45,6 +89,9 @@ public class PrefixTree {
      */
     public ArrayList<String> getWordsForPrefix(String prefix){
         //TODO: complete me
+        /*
+         * 
+         */
         return null;
     }
 
